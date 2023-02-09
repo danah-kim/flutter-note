59de74d1e4382e60fe8373aa24ae14b7b5c35956
Danah <danah.kim@kakao.com>
2023-02-10 00:16:52 +0900

    WebToon: App bar

---
+++ toonflix/lib/screens/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 2,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        title: const Text(
+          "Today's Webtoon",
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        ),
+      ),
+    );
+  }
+}
