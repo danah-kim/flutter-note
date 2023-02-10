13bc17d816142c7e81f7631178e97367c41a9b33
Danah <danah.kim@kakao.com>
2023-02-10 23:11:24 +0900

    WebToon: Webtoon Card

--- toonflix/lib/screens/home.dart
+++ toonflix/lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/lib/widgets/webtoon.dart';
 import 'package:toonflix/models/webtoon.dart';
 import 'package:toonflix/services/api.dart';
 
@@ -26,15 +27,7 @@ class Home extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return ListView.separated(
-              scrollDirection: Axis.vertical,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                var webtoon = snapshot.data![index];
-                return Text(webtoon.title);
-              },
-              separatorBuilder: (context, index) => const SizedBox(width: 20),
-            );
+            return makeList(snapshot);
           }
 
           return const Center(
@@ -42,6 +35,24 @@ class Home extends StatelessWidget {
           );
         },
       ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      itemBuilder: (context, index) {
+        var webtoon = snapshot.data![index];
+
+        return Webtoon(
+          title: webtoon.title,
+          thumb: webtoon.thumb,
+          id: webtoon.id,
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(width: 40),
     );
   }
 }
