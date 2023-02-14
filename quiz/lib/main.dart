bc7ae6433318835e64575b584f70593a102773fd
Danah <danah.kim@kakao.com>
2023-02-14 23:17:58 +0900

    Quiz: Outputting widget conditionally

--- quiz/lib/main.dart
+++ quiz/lib/main.dart
@@ -10,6 +10,20 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  final questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['Black', 'Red', 'Green', 'White'],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+    },
+    {
+      'questionText': 'What\'s your favorite food?',
+      'answers': ['Cake', 'Cookie', 'Candy', 'Jelly'],
+    }
+  ];
   var _questionIndex = 0;
 
   void _onAnswerQuestionPressed() {
@@ -20,34 +34,23 @@ class _AppState extends State<App> {
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers': ['Black', 'Red', 'Green', 'White'],
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
-      },
-      {
-        'questionText': 'What\'s your favorite food?',
-        'answers': ['Cake', 'Cookie', 'Candy', 'Jelly'],
-      }
-    ];
-
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
         title: Text('My First App'),
       ),
-      body: Column(
-        children: [
-          Question(questions[_questionIndex]['questionText'] as String),
-          ...(questions[_questionIndex]['answers'] as List<String>)
-              .map((answer) => Answer(answer, _onAnswerQuestionPressed))
-              .toList()
-        ],
-      ),
+      body: _questionIndex < questions.length
+          ? Column(
+              children: [
+                Question(questions[_questionIndex]['questionText'] as String),
+                ...(questions[_questionIndex]['answers'] as List<String>)
+                    .map((answer) => Answer(answer, _onAnswerQuestionPressed))
+                    .toList()
+              ],
+            )
+          : Center(
+              child: Text('You did it!'),
+            ),
     ));
   }
 }
