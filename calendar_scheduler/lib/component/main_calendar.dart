5877323f059513cc62e7940425dbaac5718e6849
Danah <danah.kim@kakao.com>
2023-07-03 22:24:54 +0900

    Calendar: calendar 구현

---
+++ calendar_scheduler/lib/component/main_calendar.dart
@@ -0,0 +1,62 @@
+import 'package:calendar_scheduler/const/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class MainCalendar extends StatelessWidget {
+  final DateTime selectedDate;
+  final OnDaySelected onDaySelected;
+
+  const MainCalendar({
+    super.key,
+    required this.selectedDate,
+    required this.onDaySelected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar(
+      onDaySelected: onDaySelected,
+      selectedDayPredicate: (day) =>
+          day.year == selectedDate.year &&
+          day.month == selectedDate.month &&
+          day.day == selectedDate.day,
+      focusedDay: DateTime.now(),
+      firstDay: DateTime(1800, 1, 1),
+      lastDay: DateTime(2030, 3, 14),
+      headerStyle: const HeaderStyle(
+        titleCentered: true,
+        formatButtonVisible: false,
+        titleTextStyle: TextStyle(
+          fontWeight: FontWeight.w700,
+          fontSize: 16.0,
+        ),
+      ),
+      calendarStyle: CalendarStyle(
+          isTodayHighlighted: false,
+          defaultDecoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(6.0),
+            color: LIGHT_GREY_COLOR,
+          ),
+          weekendDecoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(6.0),
+            color: LIGHT_GREY_COLOR,
+          ),
+          selectedDecoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(6.0),
+            border: Border.all(color: PRIMARY_COLOR, width: 1.0),
+          ),
+          defaultTextStyle: TextStyle(
+            fontWeight: FontWeight.w600,
+            color: DARK_GREY_COLOR,
+          ),
+          weekendTextStyle: TextStyle(
+            fontWeight: FontWeight.w600,
+            color: DARK_GREY_COLOR,
+          ),
+          selectedTextStyle: const TextStyle(
+            fontWeight: FontWeight.w600,
+            color: PRIMARY_COLOR,
+          )),
+    );
+  }
+}
---
+++ calendar_scheduler/lib/screen/home_screen.dart
@@ -0,0 +1,39 @@
+import 'package:calendar_scheduler/component/main_calendar.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime.utc(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            MainCalendar(
+              selectedDate: selectedDate,
+              onDaySelected: onDaySelected,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onDaySelected(DateTime selectedDate, DateTime focusedDate) {
+    setState(() {
+      this.selectedDate = selectedDate;
+    });
+  }
+}
